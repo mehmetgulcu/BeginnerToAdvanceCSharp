f8089f94bf2eae6b311b1e19a88ad690404d12ba
mehmetgulcu <mehmetgulcudeveloper@gmail.com>
2025-12-30 20:37:27 +0300

    Add conditional statements demo and update Program.cs
    Added _4_Conditional_Statements class to demonstrate if-else, switch, and ternary operator usage. Updated Program.cs to run this new class instead of _3_Operators.

--- BasicsOfCsharp/Program.cs
+++ BasicsOfCsharp/Program.cs
@@ -1,9 +1,11 @@
 ﻿using BasicsOfCsharp._1_Data_Types_and_Variables;
 using BasicsOfCsharp._2_Enum_and_Consts;
 using BasicsOfCsharp._3_Operators;
+using BasicsOfCsharp._4_Conditional_Statements;
 
 // var program = new _1_Data_Types_and_Variables();
 // var program = new _2_Enum_and_Consts();
-var program = new _3_Operators();
+// var program = new _3_Operators();
+var program = new _4_Conditional_Statements();
 
 program.Run();
---
+++ BasicsOfCsharp/_4_Conditional_Statements/_4_Conditional_Statements.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicsOfCsharp._4_Conditional_Statements
+{
+    public class _4_Conditional_Statements
+    {
+        public void Run()
+        {
+            int number = 10;
+            // If-Else Statement
+            if (number > 0)
+            {
+                Console.WriteLine("The number is positive.");
+            }
+            else if (number < 0)
+            {
+                Console.WriteLine("The number is negative.");
+            }
+            else
+            {
+                Console.WriteLine("The number is zero.");
+            }
+            // Switch Statement
+            string day = "Monday";
+            switch (day)
+            {
+                case "Monday":
+                    Console.WriteLine("Start of the work week.");
+                    break;
+                case "Friday":
+                    Console.WriteLine("End of the work week.");
+                    break;
+                default:
+                    Console.WriteLine("Midweek days.");
+                    break;
+            }
+            // Ternary Operator
+            string result = (number % 2 == 0) ? "Even" : "Odd";
+            Console.WriteLine($"The number is {result}.");
+        }
+    }
+}
