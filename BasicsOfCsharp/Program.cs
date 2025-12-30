61bf8c4be0d63edc060f6bbfca7f983e7b8db03a
mehmetgulcu <mehmetgulcudeveloper@gmail.com>
2025-12-30 20:39:45 +0300

    _5_Loops(

--- BasicsOfCsharp/Program.cs
+++ BasicsOfCsharp/Program.cs
@@ -1,4 +1,5 @@
-﻿using BasicsOfCsharp._1_Data_Types_and_Variables;
+﻿using _1_BasicsOfCsharp._5_Loops;
+using BasicsOfCsharp._1_Data_Types_and_Variables;
 using BasicsOfCsharp._2_Enum_and_Consts;
 using BasicsOfCsharp._3_Operators;
 using BasicsOfCsharp._4_Conditional_Statements;
@@ -6,6 +7,7 @@ using BasicsOfCsharp._4_Conditional_Statements;
 // var program = new _1_Data_Types_and_Variables();
 // var program = new _2_Enum_and_Consts();
 // var program = new _3_Operators();
-var program = new _4_Conditional_Statements();
+// var program = new _4_Conditional_Statements();
+var program = new _5_Loops();
 
 program.Run();
---
+++ BasicsOfCsharp/_5_Loops/_5_Loops.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1_BasicsOfCsharp._5_Loops
+{
+    public class _5_Loops
+    {
+        public void Run()
+        {
+            // For Loop
+            Console.WriteLine("For Loop:");
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine($"Iteration {i}");
+            }
+            // While Loop
+            Console.WriteLine("\nWhile Loop:");
+            int j = 1;
+            while (j <= 5)
+            {
+                Console.WriteLine($"Iteration {j}");
+                j++;
+            }
+            // Do-While Loop
+            Console.WriteLine("\nDo-While Loop:");
+            int k = 1;
+            do
+            {
+                Console.WriteLine($"Iteration {k}");
+                k++;
+            } while (k <= 5);
+            // Foreach Loop
+            Console.WriteLine("\nForeach Loop:");
+            string[] fruits = { "Apple", "Banana", "Cherry" };
+            foreach (string fruit in fruits)
+            {
+                Console.WriteLine(fruit);
+            }
+        }
+    }
+}
