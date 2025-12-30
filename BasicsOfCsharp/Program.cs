d1f6b3fc8c91ec3589a4862546b29b980c830439
mehmetgulcu <mehmetgulcudeveloper@gmail.com>
2025-12-30 20:48:55 +0300

    _6_IO

--- BasicsOfCsharp/Program.cs
+++ BasicsOfCsharp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using _1_BasicsOfCsharp._5_Loops;
+using _1_BasicsOfCsharp._6_IO;
 using BasicsOfCsharp._1_Data_Types_and_Variables;
 using BasicsOfCsharp._2_Enum_and_Consts;
 using BasicsOfCsharp._3_Operators;
@@ -8,6 +9,7 @@ using BasicsOfCsharp._4_Conditional_Statements;
 // var program = new _2_Enum_and_Consts();
 // var program = new _3_Operators();
 // var program = new _4_Conditional_Statements();
-var program = new _5_Loops();
+// var program = new _5_Loops();
+var program = new _6_IO();
 
 program.Run();
---
+++ BasicsOfCsharp/_6_IO/_6_IO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1_BasicsOfCsharp._6_IO
+{
+    public class _6_IO
+    {
+        public void Run()
+        {
+            Main(new string[] { });
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Input/Output in C#");
+            // Example of reading input from the console
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name + "!");
+        }
+    }
+}
